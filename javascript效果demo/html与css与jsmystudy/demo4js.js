f5c482aaee863b5e55d9b9650f1a675e8649c9e3
15110671453 <15110671453@163.com>
2017-03-03 10:24:25 +0800

    Revert "Revert "javascript效果demo""
    This reverts commit 12d7169e98bd2594415cf25004fc68923c93c794.

---
+++ javascript效果demo/html与css与jsmystudy/demo4js.js
@@ -0,0 +1,58 @@
+window.onload = function()
+{
+  /*
+  $().getClass('personalcenter').hover(over,out);
+  设计目标
+  */ 
+  //个人中心
+	$().getClass('personalcenter').hover(function(){$().getClass('member-ul').show(); },function(){ $().getClass('member-ul').hidden();});
+
+
+
+
+/*
+window.onresize = function () {
+	// body...
+	//登录框
+var top = (document.documentElement.clientHeight-250)/2;
+var left = (document.documentElement.clientWidth-350)/2;
+$().getid('login').css('top',top+'px').css('left',left+'px');	
+
+}
+设计目标
+*/
+
+	 var login1 = $().getid('login2');
+
+
+     var screen = $().getid('screen');
+      login1.center(350,250);
+   $().resize(function(){ 
+    login1.center(350,250);
+    if (login1.css('display')=='block') {
+      screen.lock();
+    }
+
+
+  });
+
+    $().getClass('login').click(function() { login1.css('display','block');
+       screen.lock();
+
+  });
+
+  $().getClass('close').click(function () {login1.css('display','none');
+
+       screen.unlock();
+  });
+
+
+
+
+
+
+
+}
+
+
+
