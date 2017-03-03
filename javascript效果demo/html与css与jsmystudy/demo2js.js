12d7169e98bd2594415cf25004fc68923c93c794
15110671453 <15110671453@163.com>
2017-03-03 10:21:36 +0800

    Revert "javascript效果demo"
    This reverts commit d4f6f5fff538f10ba4a592afde4b324216f87e79.

--- javascript效果demo/html与css与jsmystudy/demo2js.js
+++
@@ -1,9 +0,0 @@
-window.onload = function()
-{
-  /*
-  $().getClass('personalcenter').hover(over,out);
-  设计目标
-  */ 
-	$().getClass('personalcenter').hover(function(){$().getClass('member-ul').show(); },function(){ $().getClass('member-ul').hidden();});
-	
-}
